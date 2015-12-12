acad745eedd1e6f711140841dec406336840b178
ValdemarLvov <lion-ret@ya.ru>
2015-12-13 00:21:18 +0200

    Map with tanks
    Map1+tanks

---
+++ Client/Assets/Scripts/AMMO.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AMMO : MonoBehaviour {
+    float timer = 0f;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (timer < 100f)
+            timer += 1f;
+        else Destroy(gameObject);
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "wall" || col.gameObject.tag == "tank")
+        {
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+        }
+    }
+}
