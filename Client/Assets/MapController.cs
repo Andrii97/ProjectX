839d4a9e67327b41531ae413d9b1e474506ca7c8
KirillRedin <kirillredin.ua@gmail.com>
2015-12-19 01:25:40 +0200

    Ending messages

--- Client/Assets/MapController.cs
+++ Client/Assets/MapController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class MapController : MonoBehaviour {
 
-    public GameObject BotTank, MyTank, ammo, test;
+    public GameObject BotTank, MyTank, ammo, test, Victory, Death;
     private Vector3 temp;
+    public int status = 0, WinTrigger;
+    public float step;
+    public Color EndMsgColor;
 
     // Use this for initialization
     void Start () {
-
-        CreateChild();
+        
     }
 
     void Swap()
@@ -46,19 +48,78 @@ public class MapController : MonoBehaviour {
         GameObject newAmmo = (GameObject)Instantiate(ammo, MyTank.transform.position, MyTank.transform.rotation);
         newAmmo.GetComponent<Rigidbody>().AddForce(force * 500f, ForceMode.Force);
     }
-    
-    void CreateChild()
+
+    void EndMessage(GameObject obj)
     {
-        GameObject canvas = GameObject.Find("Canvas");
-        GameObject button = Instantiate(test) as GameObject;
-        button.transform.parent = canvas.transform;
+        if (status == 1)
+        {
+            if (EndMsgColor.a < 0.5)
+            {
+                EndMsgColor.a += step / 100;
+                obj.GetComponent<Renderer>().material.SetColor("_TintColor", EndMsgColor);
+            }
+            else
+                status = 2;
+        }
+        else if (status == 2)
+        {
+            if (EndMsgColor.a > 0.4)
+            {
+                EndMsgColor.a -= step / 1000;
+                obj.GetComponent<Renderer>().material.SetColor("_TintColor", EndMsgColor);
+            }
+            else
+                status = 3;
+        }
+        else if (status == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                status = 0;
+                WinTrigger = 0;
+                EndMsgColor.a = 0;
+                obj.GetComponent<Renderer>().material.SetColor("_TintColor", EndMsgColor);
+                Application.LoadLevel("menu");
+            }
+            else if (EndMsgColor.a < 0.5)
+            {
+                EndMsgColor.a += step / 1000;
+                obj.GetComponent<Renderer>().material.SetColor("_TintColor", EndMsgColor);
+            }
+            else
+                status = 2;
+        }
     }
 
     // Update is called once per frame
     void Update () {
+
+        if (Input.GetKeyDown(KeyCode.F1) && WinTrigger == 0)
+        {
+            status = 1;
+            WinTrigger = 1;
+            EndMsgColor = Victory.GetComponent<Renderer>().material.GetColor("_TintColor");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2) && WinTrigger == 0)
+        {
+            status = 1;
+            WinTrigger = 2;
+            EndMsgColor = Death.GetComponent<Renderer>().material.GetColor("_TintColor");
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CreateMyAmmo();
         }
+        
+        if (WinTrigger == 1)
+        {
+            EndMessage(Victory);
+        }
+        else
+        {
+            EndMessage(Death);            
+        }        
     }
 }
